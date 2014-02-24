fb2c67d87d58e95453660ee6f357e9b1f4770ea9
Jason Thigpen <darwin@senet.us>
2014-02-24 13:25:55 -0800

    Sleep for less overall time

--- simple.rb
+++ simple.rb
@@ -3,11 +3,13 @@
 set :bind, '0.0.0.0'
 
 get '/' do
-   sleep(rand(2))
+   wait_for = 0.01
+   sleep(rand(0.0..wait_for))
    'Hello world!'
 end
 
 get '/test' do
-  sleep(rand(1))
+  wait_for = 0.1
+  sleep(rand(0.0..wait_for))
   'I was tired'
 end
